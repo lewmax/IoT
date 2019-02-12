76b4d42e7f1bcfbb5b7c8be0482cdc40a44e7525
lewmax <mr.lewmax@gmail.com>
2019-02-12 10:41:18 +0200

    corrected constructors, names of variables

--- lab1.java
+++ lab1.java
@@ -1,27 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        Printer HP = new Printer("HP", 10, 3500, "A1234", 3.5);
-        Printer ACER = new Printer("Acer", 15, 5000, "B2019", 2.8, "Black and white", 1000);
-        Printer PANASONIC = new Printer();
+        Printer hp = new Printer("HP", 10, 3500, "A1234", 3.5);
+        Printer acer = new Printer("Acer", 15, 5000, "B2019", 2.8, "Black and white", 1000);
+        Printer panasonic = new Printer();
 
-        System.out.println(HP.toString());
-        System.out.println(ACER.toString());
-        System.out.println(PANASONIC.toString());
+        System.out.println(hp.toString());
+        System.out.println(acer.toString());
+        System.out.println(panasonic.toString());
 
-        PANASONIC.setName("Panasonic");
-        PANASONIC.setNumberOfPagesPerMinute(20);
-        PANASONIC.setPrice(6125);
-        PANASONIC.setModel("C9876");
-        PANASONIC.setWeight(4.1);
-        PANASONIC.setColorOfPrinting("Colored");
-        PANASONIC.setAmountOfPages(2000);
+        panasonic.setName("Panasonic");
+        panasonic.setNumberOfPagesPerMinute(20);
+        panasonic.setPrice(6125);
+        panasonic.setModel("C9876");
+        panasonic.setWeight(4.1);
+        panasonic.setColorOfPrinting("Colored");
+        panasonic.setAmountOfPages(2000);
 
-        System.out.println(PANASONIC.toString());
+        System.out.println(panasonic.toString());
 
-        System.out.println("Static weight: " + HP.printWeight());
-        System.out.println("Static weight: " + ACER.printWeight());
-        System.out.println("Static weight: " + PANASONIC.printWeight());
+        System.out.println("Static weight: " + hp.printWeight());
+        System.out.println("Static weight: " + acer.printWeight());
+        System.out.println("Static weight: " + panasonic.printWeight());
 
         System.out.println("Static method: " + Printer.printStaticWeight());
     }
@@ -53,18 +53,20 @@ public class Main {
             this(null, 0, 0, null, 0, null, 0);
         }
 
-        public static double printStaticWeight(){
+        public static double printStaticWeight() {
             return weight;
         }
 
         public double printWeight() {
             return weight;
         }
+
         public String toString() {
             return "Printer: " + "Name: " + name + ", NumberOfPagesPerMinute: " + numberOfPagesPerMinute +
                     ", Price: " + price + ", Model: " + model + ", Weight: " + weight +
                     ", ColorOfPrinting: " + colorOfPrinting + ", AmountOfPages: " + amountOfPages;
         }
+
         public void resetValues(String name, int numberOfPagesPerMinute, double price, String model, double weight, String colorOfPrinting, int amountOfPages) {
             this.name = name;
             this.numberOfPagesPerMinute = numberOfPagesPerMinute;
@@ -74,45 +76,59 @@ public class Main {
             this.colorOfPrinting = colorOfPrinting;
             this.amountOfPages = amountOfPages;
         }
+
         public void setName(String newName) {
             this.name = newName;
         }
+
         public void setNumberOfPagesPerMinute(int newNumberOfPagesPerMinute) {
             this.numberOfPagesPerMinute = newNumberOfPagesPerMinute;
         }
+
         public void setPrice(double newPrice) {
             this.price = newPrice;
         }
+
         public void setModel(String newModel) {
             this.model = newModel;
         }
+
         public void setWeight(double newWeight) {
             this.weight = newWeight;
         }
+
         public void setColorOfPrinting(String newColorOfPrinting) {
             this.colorOfPrinting = newColorOfPrinting;
         }
+
         public void setAmountOfPages(int newAmountOfPages) {
             this.amountOfPages = newAmountOfPages;
         }
+
         public String getName() {
             return this.name;
         }
+
         public int getNumberOfPagesPerMinute() {
             return this.numberOfPagesPerMinute;
         }
+
         public double getPrice() {
             return this.price;
         }
+
         public String getModel() {
             return this.model;
         }
+
         public double getWeight() {
             return this.weight;
         }
+
         public String getColorOfPrinting() {
             return this.colorOfPrinting;
         }
+
         public double getAmountOfPages() {
             return this.amountOfPages;
         }
